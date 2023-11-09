08583096296dbf3836cb7c916bb6ace1d862ff56
Daniel Mendes <dm099150@gmail.com>
2023-11-09 11:19:32 -0300

    criação do codigo que exibe a mensagen hello world

---
+++ Hello.asm
@@ -0,0 +1,13 @@
+section .text
+    global _start           ;precisa ser adicionado para o  gerador do executavel saiba onde começa
+
+_start:
+  mov edx,len               ;tamanho do texto. len(lenth-> comprimento)
+  mov ecx,msg               ;mensagen de texto para exibir
+  mov ebx,1                 ;descritor de arquivo
+  mov eax,4                 ;chamada para escrever o texto na tela
+  int -x80                  ;chama o terminal para exibir o texto
+
+  section .data             ;seçao de dados 
+  msg db 'hello, world!'     :mensagenqe deve aparecer
+  len equ $ - msg             ;obtem a quantidade de caracteres da mensagen
